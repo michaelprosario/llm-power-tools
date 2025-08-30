288dd4267ef82998e4ba7fec0f0b794945fb897c
Michael Rosario <michael.p.rosario@gmail.com>
2025-08-30 02:24:14 +0000

    feat: add initial implementation of Google search agent

---
+++ adkAgents/google_search/agent.py
@@ -0,0 +1,33 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from google.adk.agents import Agent
+from google.adk.runners import Runner
+from google.adk.sessions import InMemorySessionService
+from google.adk.tools import google_search
+from google.genai import types
+
+APP_NAME="google_search_agent"
+USER_ID="user1234"
+SESSION_ID="1234"
+
+
+root_agent = Agent(
+    name="basic_search_agent",
+    model="gemini-2.0-flash",
+    description="Agent to answer questions using Google Search.",
+    instruction="I can answer your questions by searching the internet. Just ask me anything!",
+    # google_search is a pre-built tool which allows the agent to perform Google searches.
+    tools=[google_search]
+)
